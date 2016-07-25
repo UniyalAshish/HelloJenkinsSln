60397124ac89b475e733e5357a3d2e9c52547c77
Ashish Uniyal <ashish.uniyal@target.com>
2016-07-25 14:49:23 +0530

    fixed test case

--- MyConsoleAppTest/Class1.cs
+++ MyConsoleAppTest/Class1.cs
@@ -15,7 +15,7 @@ namespace MyConsoleAppTest
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(4, Add(2, 2));
         }
 
         int Add(int x, int y)
